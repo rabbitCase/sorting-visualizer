3dd77a9a39490ca7d061a4e8924d0c6864b51197
rabbitCase <gnair3004@gmail.com>
2025-06-03 08:59:15 +0530

    reduced global dependency pollution

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-let stopFlag = false;
-let arraySize = document.getElementById('input-size');
-arraySize.value = 10;
-
 const simulation = (function sortingVisualizer(){
     const arrayContainer = document.getElementById('array-container');
-    const start = document.getElementById('start-button');
-    const stop = document.getElementById('stop-button');
+
+    //scoped variables and constants
+    let arr = [];
+    let stopFlag = false;
+    let target = "";
+    let arraySize = document.getElementById('input-size');
+    arraySize.value = 10;
     const dataSize = arraySize.value
+
+    function setSpeedValue(){
+        document.getElementById('value').textContent = document.getElementById('speed').value;
+    }
 
     function createPillar(array){
         array.forEach(value => {
@@ -40,15 +45,8 @@ const simulation = (function sortingVisualizer(){
     async function timer(time){
         return await new Promise(resolve => setTimeout(resolve, time));
     }
-
-    async function setSimulation(arr){
-        createPillar(arr);
-        await timer(800)
-        changePillarHeight(arr);
-    }
-
+    
     async function bubbleSort(arr){
-
         for(let j = 0 ; j < arr.length ; j++){
             for(let i = 0 ; i < arr.length ; i++){
                 if(stopFlag){
@@ -63,7 +61,6 @@ const simulation = (function sortingVisualizer(){
                     changePillarColor(i, 'red');
                     changePillarColor(i + 1, 'red');
                     await timer(`${(10-speed) * (71 - dataSize)}`);
-                    
                     changePillarHeight(arr); 
                     changePillarColor(i, 'yellow');
                     changePillarColor(i + 1, 'yellow');
@@ -74,7 +71,6 @@ const simulation = (function sortingVisualizer(){
     }
 
     async function selectionSort(arr){
-
         for(let j = 0 ; j < arr.length ; j++){
             for(let i = j + 1 ; i < arr.length ; i++){
                 if(stopFlag){
@@ -97,26 +93,61 @@ const simulation = (function sortingVisualizer(){
             }
         }
     }
-    return {createPillar, changePillarHeight, changePillarColor, timer, setSimulation, bubbleSort, selectionSort, arrayContainer, start, stop};
-})();
 
-//main
-document.getElementById('value').textContent = document.getElementById('speed').value;
-document.getElementById('start-button').disabled = true;
-document.getElementById('stop-button').disabled = true;
+    async function insertionSort(arr){
+        for(let i = 1; i < arr.length; i++){
+            let key = arr[i];
+            let j = i - 1;
+            for(; j >= 0; j--){
+                if(stopFlag){
+                    return;
+                }
+                let speed = document.getElementById('speed').value
+                if(arr[j] > key){
+                    arr[j+1] = arr[j];
+                    changePillarColor(i, 'red');
+                    changePillarColor(j, 'red');
+                    await timer(`${(10-speed) * (71 - dataSize)}`); 
+                    changePillarColor(i, 'yellow');
+                    changePillarColor(j, 'yellow');
+                    await timer(`${(10-speed) * (71 - dataSize)}`)
+                }
+                else break;
+            }
+            arr[j + 1] = key;
+            changePillarHeight(arr);
+            await timer(`${(10-speed) * (71 - dataSize)}`)
+        }
+    }
 
-let arr = [];
-let target = "";
+    function buttonTargettable(id, status){
+        const button = document.getElementById(id);
+        if (button) {
+            button.disabled = !status;
+        } else {
+            console.warn(`"${id}" not found.`);
+        }
+    }
 
-document.getElementById("input-container").addEventListener("click", async function(event) {
-    if(event.target.classList.contains("sim-button")) {
+    function allButtonsTargettable(status){
+        const buttonList = document.querySelectorAll('.sim-button');
+        buttonList.forEach(button => button.disabled = !status);
+    }
+
+    async function setPillars(arr){//set pillar heights according to the randomized array
+        createPillar(arr);
+        await timer(800)
+        changePillarHeight(arr);
+    }
+
+    async function readyData(event){//initialize a random array and ready the pillars to start the simulation
         if(arraySize.value < 1 || arraySize.value > 70){
             alert("input range = [1,70]");
             return;
         }
 
-        simulation.arrayContainer.innerHTML = "";
-        document.getElementById('start-button').disabled = true;
+        arrayContainer.innerHTML = "";
+        buttonTargettable('start-button', false);
         arr= [];
         let inputSize = document.getElementById('input-size').value;
         while(inputSize --){
@@ -124,46 +155,76 @@ document.getElementById("input-container").addEventListener("click", async funct
             arr.push(randomNumber);
         }
     
-        await simulation.setSimulation(arr);
+        await setPillars(arr);
         target = event.target.id;
-        document.getElementById('start-button').disabled = false;
+        buttonTargettable('start-button', true);
     }
-    
+
+    async function applyAlgorithm(){
+        stopFlag = false;
+        if(arraySize.value < 1 || arraySize.value > 70){
+            alert("input range = [1,70]");
+            return;
+        }
+
+        allButtonsTargettable(false);
+        buttonTargettable('stop-button', true);
+
+        if(target == 'bubble-sort-button'){
+            await bubbleSort(arr);
+        }
+        else if(target == 'selection-sort-button'){
+            await selectionSort(arr);
+        }
+        else{
+            await insertionSort(arr);
+        }
+
+        allButtonsTargettable(true);
+        buttonTargettable('start-button', false);
+        buttonTargettable('stop-button', false);
+    }
+
+    function markComplete(){
+        const pillars = document.querySelectorAll('.pillar');
+        if(!stopFlag){
+            pillars.forEach(pillar => {
+                pillar.style.backgroundColor = 'rgb(111, 255, 98)';
+            })
+        }
+    }
+
+    function stopOperations(){
+        stopFlag = true;
+    }
+
+    return {
+        setSpeedValue, createPillar, changePillarHeight, changePillarColor, timer, bubbleSort, selectionSort, insertionSort, buttonTargettable, setPillars, readyData, applyAlgorithm, markComplete, stopOperations
+    };
+})();
+
+//main
+const startButton = document.getElementById('start-button');
+const stopButton = document.getElementById('stop-button');
+
+simulation.setSpeedValue();
+
+document.getElementById('speed').addEventListener('input', () => {//respond to changes in  speed value
+    simulation.setSpeedValue();
 });
 
-simulation.start.addEventListener('click', async () => {
-    stopFlag = false;
-    if(arraySize.value < 1 || arraySize.value > 70){
-        alert("input range = [1,70]");
-        return;
-    }
+simulation.buttonTargettable('start-button', false);
+simulation.buttonTargettable('stop-button', false);
 
-    const buttonList = document.querySelectorAll('.sim-button');
-    buttonList.forEach(button => button.disabled = true);
-
-    document.getElementById('stop-button').disabled = false;
-
-    if(target == 'selection-sort-button'){
-        await simulation.selectionSort(arr);
+document.getElementById("input-container").addEventListener("click", async function(event) {
+    if(event.target.classList.contains("sim-button")) {
+        await simulation.readyData(event);
     }
-    else{
-        await simulation.bubbleSort(arr);
-    }
-    
-    const pillars = document.querySelectorAll('.pillar');
-    if(!stopFlag){
-        pillars.forEach(pillar => {
-            pillar.style.backgroundColor = 'rgb(111, 255, 98)';
-        })
-    }
-    buttonList.forEach(button => button.disabled = false);
-    document.getElementById('start-button').disabled = true;
-    document.getElementById('stop-button').disabled = true;
 });
 
-
-simulation.stop.addEventListener('click', () => stopFlag = true);
-
-document.getElementById('speed').addEventListener('input', () => {
-    document.getElementById('value').textContent = document.getElementById('speed').value;
+startButton.addEventListener('click', async () => {
+    await simulation.applyAlgorithm();
+    simulation.markComplete();
 });
+
+stopButton.addEventListener('click', () => simulation.stopOperations());
